717a058a9d0c8eb1e298225fd01f82edf0ee7ef4
eugene1204 <s1061001@naver.com>
2022-12-16 21:22:59 +0900

    Spring Web

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,8 @@ package hello.hellospring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,4 +12,32 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 바디부에 데이터를 직접 넣어주겠습니다.
+    // 뷰가 없이 문자가 그대로 넘어간당
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // ex) hello spring
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        // 자바 빈 규약 
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        } }
+
 }
