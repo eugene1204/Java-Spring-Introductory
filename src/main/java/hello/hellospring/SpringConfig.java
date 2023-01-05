cc4bc7f0aa7a7f6ebb61229ea77f1f23226c8325
eugene1204 <s1061001@naver.com>
2023-01-05 18:41:37 +0900

    Spring Data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,22 +11,28 @@ import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.em = em;
-        this.dataSource = dataSource;
+    private final MemberRepository memberRepository;
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.em = em;
+//        this.dataSource = dataSource;
+//    }
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
 }
